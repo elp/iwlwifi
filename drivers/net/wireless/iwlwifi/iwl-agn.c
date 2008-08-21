067fe193d6c27a555ebd74c26ff774e5fffcf87e
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2008-08-21 17:39:15 +0300

    iwlwifi: fix strict_strtoul error checking
    This patch fixes handling of strcit_strtoul return value
    "0 == sucess".
    
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: Zhu Yi <yi.zhu@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-agn.c
+++ drivers/net/wireless/iwlwifi/iwl-agn.c
@@ -3737,7 +3737,7 @@ static ssize_t store_flags(struct device *d,
 	unsigned long val;
 	u32 flags;
 	int ret = strict_strtoul(buf, 0, &val);
-	if (!ret)
+	if (ret)
 		return ret;
 	flags = (u32)val;
 
@@ -3776,7 +3776,7 @@ static ssize_t store_filter_flags(struct device *d,
 	unsigned long val;
 	u32 filter_flags;
 	int ret = strict_strtoul(buf, 0, &val);
-	if (!ret)
+	if (ret)
 		return ret;
 	filter_flags = (u32)val;
 
@@ -3916,7 +3916,7 @@ static ssize_t store_power_level(struct device *d,
 	}
 
 	ret = strict_strtoul(buf, 10, &mode);
-	if (!ret)
+	if (ret)
 		goto out;
 
 	ret = iwl_power_set_user_mode(priv, mode);
