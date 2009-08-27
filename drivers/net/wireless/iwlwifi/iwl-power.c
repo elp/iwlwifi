93521dd2d3b11c8e63b95aea6ad6af2100a7ae9b
Johannes Berg <johannes@sipsolutions.net>
2009-08-27 15:06:19 -0700

    iwlwifi: put card to sleep when unassociated/idle
    When mac80211 signals that the card is idle, we can instruct
    the microcode to sleep. mac80211 will wake it up for the
    association period, and after associating will instruct us
    to go to PS mode (which we do again, but mac80211 doesn't
    know that we use PS mode for idle).
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-power.c
+++ drivers/net/wireless/iwlwifi/iwl-power.c
@@ -292,8 +292,9 @@ int iwl_power_update_mode(struct iwl_priv *priv, bool force)
 	else
 		dtimper = 1;
 
-	/* TT power setting overwrites everything */
-	if (tt->state >= IWL_TI_1)
+	if (priv->hw->conf.flags & IEEE80211_CONF_IDLE)
+		iwl_static_sleep_cmd(priv, &cmd, IWL_POWER_INDEX_5, 20);
+	else if (tt->state >= IWL_TI_1)
 		iwl_static_sleep_cmd(priv, &cmd, tt->tt_power_mode, dtimper);
 	else if (!enabled)
 		iwl_power_sleep_cam_cmd(priv, &cmd);
