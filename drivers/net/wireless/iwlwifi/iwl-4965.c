47149329df51c238b02d4313ec4d7a5df81c02fb
Johannes Berg <johannes.berg@intel.com>
2010-04-28 08:44:52 -0700

    iwlwifi: manage IBSS station properly
    Currently iwlwifi will eventually exhaust the station
    table when adding the BSSID station for IBSS mode,
    unless the interface is set down.
    
    The new mac80211 ibss joined/left notification allows
    us to fix that easily by moving the code to add the
    IBSS station to the notification, and also adding
    code to remove it again when we leave the IBSS.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-4965.c
+++ drivers/net/wireless/iwlwifi/iwl-4965.c
@@ -2211,6 +2211,7 @@ static struct iwl_lib_ops iwl4965_lib = {
 		.set_ct_kill = iwl4965_set_ct_threshold,
 	},
 	.add_bcast_station = iwl_add_bcast_station,
+	.manage_ibss_station = iwlagn_manage_ibss_station,
 	.debugfs_ops = {
 		.rx_stats_read = iwl_ucode_rx_stats_read,
 		.tx_stats_read = iwl_ucode_tx_stats_read,
