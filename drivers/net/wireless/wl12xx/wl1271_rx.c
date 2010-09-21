72824a58a9ecf251f7593c3afbc936667461ac25
John W. Linville <linville@tuxdriver.com>
2010-09-21 11:28:00 -0400

    Revert "wl1271: avoid redundant memcpy of rx_status"
    This reverts commit e94abecf97631a8f05e00c0c56a37a5cccba81a9.

--- drivers/net/wireless/wl12xx/wl1271_rx.c
+++ drivers/net/wireless/wl12xx/wl1271_rx.c
@@ -76,6 +76,7 @@ static void wl1271_rx_status(struct wl1271 *wl,
 
 static void wl1271_rx_handle_data(struct wl1271 *wl, u32 length)
 {
+	struct ieee80211_rx_status rx_status;
 	struct wl1271_rx_descriptor *desc;
 	struct sk_buff *skb;
 	u16 *fc;
@@ -108,13 +109,14 @@ static void wl1271_rx_handle_data(struct wl1271 *wl, u32 length)
 	if ((*fc & IEEE80211_FCTL_STYPE) == IEEE80211_STYPE_BEACON)
 		beacon = 1;
 
-	wl1271_rx_status(wl, desc, IEEE80211_SKB_RXCB(skb), beacon);
+	wl1271_rx_status(wl, desc, &rx_status, beacon);
 
 	wl1271_debug(DEBUG_RX, "rx skb 0x%p: %d B %s", skb, skb->len,
 		     beacon ? "beacon" : "");
 
 	skb_trim(skb, skb->len - desc->pad_len);
 
+	memcpy(IEEE80211_SKB_RXCB(skb), &rx_status, sizeof(rx_status));
 	ieee80211_rx_ni(wl->hw, skb);
 }
 
