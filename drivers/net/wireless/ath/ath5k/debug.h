f1083c0787dddc85531832b4393289eb21a4cab7
John W. Linville <linville@tuxdriver.com>
2010-09-27 15:50:42 -0400

    Revert "ath5k: Add bssid mask and rxfilter to debugfs."
    This reverts commit 296e4ca6801ea28e0f809920e83a570c65de935b.

--- drivers/net/wireless/ath/ath5k/debug.h
+++ drivers/net/wireless/ath/ath5k/debug.h
@@ -75,7 +75,6 @@ struct ath5k_dbg_info {
 	struct dentry		*debugfs_beacon;
 	struct dentry		*debugfs_reset;
 	struct dentry		*debugfs_antenna;
-	struct dentry		*debugfs_misc;
 	struct dentry		*debugfs_frameerrors;
 	struct dentry		*debugfs_ani;
 	struct dentry		*debugfs_queue;
