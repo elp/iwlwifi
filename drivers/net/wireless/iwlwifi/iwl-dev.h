a1e38108dad589be2551b2186339e8b33faa6595
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2011-05-31 08:52:10 +0300

    iwlagn: add get_dev to iwl_bus_ops
    Bus specific layer must know how to return the struct device* of the device.
    Implement that as a callback of iwl_bus_ops and use that callback instead of
    using the priv->pdev pointer which is meant to disappear soon.
    
    Since the struct device * is needed in hot path, iwl_bus holds a pointer to it
    instead of calling get_dev all the time.
    
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-dev.h
+++ drivers/net/wireless/iwlwifi/iwl-dev.h
@@ -1195,9 +1195,11 @@ struct iwl_bus;
 /**
  * struct iwl_bus_ops - bus specific operations
  * @set_drv_data: set the priv pointer to the bus layer
+ * @get_dev: returns the device struct
  */
 struct iwl_bus_ops {
 	void (*set_drv_data)(struct iwl_bus *bus, void *priv);
+	struct device *(*get_dev)(const struct iwl_bus *bus);
 };
 
 struct iwl_bus {
@@ -1206,6 +1208,7 @@ struct iwl_bus {
 
 	/* Common data to all buses */
 	struct iwl_priv *priv; /* driver's context */
+	struct device *dev;
 	struct iwl_bus_ops *ops;
 };
 
