1a8bc78860eb21cc0db647ff891ba8afbf070b91
Meenakshi Venkataraman <meenakshi.venkataraman@intel.com>
2012-04-30 16:28:09 -0700

    iwlwifi: fix a potential race in receive buffer allocation
    The driver can potentially unmap pages that
    have not been mapped yet. Fix this race
    condition.
    
    Cc: stable@vger.kernel.org
    Reported-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Signed-off-by: Meenakshi Venkataraman <meenakshi.venkataraman@intel.com>
    Signed-off-by: Wey-Yi Guy <wey-yi.w.guy@intel.com>

--- drivers/net/wireless/iwlwifi/iwl-trans-pcie-rx.c
+++ drivers/net/wireless/iwlwifi/iwl-trans-pcie-rx.c
@@ -313,7 +313,6 @@ static void iwlagn_rx_allocate(struct iwl_trans *trans, gfp_t priority)
 		spin_unlock_irqrestore(&rxq->lock, flags);
 
 		BUG_ON(rxb->page);
-		rxb->page = page;
 		/* Get physical address of the RB */
 		rxb->page_dma = dma_map_page(trans->dev, page, 0,
 				PAGE_SIZE << trans_pcie->rx_page_order,
@@ -322,6 +321,9 @@ static void iwlagn_rx_allocate(struct iwl_trans *trans, gfp_t priority)
 		BUG_ON(rxb->page_dma & ~DMA_BIT_MASK(36));
 		/* and also 256 byte aligned! */
 		BUG_ON(rxb->page_dma & DMA_BIT_MASK(8));
+
+		/* Page *must* be mapped before before updating the rxb. */
+		rxb->page = page;
 
 		spin_lock_irqsave(&rxq->lock, flags);
 
